c2c34a2665035ae7f60a93a28f6fde494647f7cb
marvan <68455314+marvan87i@users.noreply.github.com>
2021-01-18 18:56:45 -0600

    Pagina web de News con scroll Reveal

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-Button').addEventListener('click', () =>{
+    document.querySelector('.navMenu').classList.toggle('show')
+});
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase', {delay:400});
+ScrollReveal().reveal('.newsCards', {delay:400});
+ScrollReveal().reveal('.bannerCards', {delay:400});
+ScrollReveal().reveal('.bannerTwo', {delay:400});
+ScrollReveal().reveal('.social', {delay:400});
